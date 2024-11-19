3a814301191f6266c792196a5affd8ad8f9de652
Theodore Hammar <theodore.hammar@elev.ga.ntig.se>
2024-11-19 10:16:02 +0100

    Fixat lite med output från koden

--- src/Armor.java
+++ src/Armor.java
@@ -15,6 +15,10 @@ public class Armor extends Item {
         String helmetChoice = sc.nextLine();
         if (helmetChoice.equalsIgnoreCase("y")) {
             backpack.addItem(new Armor(1, "helmet of rizz", 5.2F));
+        } else if (helmetChoice.equalsIgnoreCase("n")) {
+            System.out.println("You chose to not pick up the hemlet");
+        } else {
+            System.out.println("Faulty inout, try again");
         }
     }
 
